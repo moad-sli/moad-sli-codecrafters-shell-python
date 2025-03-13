3a7aa483411148072595dc863d45844942958aae
slimane mouad <mslimane@leyton.com>
2025-03-13 15:35:15 +0000

    add command echo

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ def command_not_found(user_input):
     if user_input == "exit 0":
         return 0
     elif user_input.startswith("echo "):
-        print(user_input.removeprefix("echo "))
-        return 0
+        print(user_input.removeprefix("echo ").strip())
+        return 1
     else:
         print(f"{user_input}: command not found")
         return 1
