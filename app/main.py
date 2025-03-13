c143592df65660ff01f9fc3d0d47fd2dbac08ac0
slimane mouad <mslimane@leyton.com>
2025-03-13 15:25:51 +0000

    add command exit 0

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import sys
 
 def command_not_found(user_input):
-    sys.stdout.write("$ ")
     if user_input == "exit 0":
         return 0
     else:
         print(f"{user_input}: command not found")
         return 1
+
+def take_input():
+    sys.stdout.write("$ ")
+    return input()
 def main():
 
     # Wait for user input
-    sys.stdout.write("$ ")
-    command=input()
+    command=take_input()
     while command_not_found(command):
-
-        command=input()
+        command=take_input()
 
 if __name__ == "__main__":
     main()
