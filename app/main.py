ffe4ebb97f589beaafa47078acb549a19bd8cea9
slimane mouad <mslimane@leyton.com>
2025-03-13 15:17:34 +0000

    Add command not found check

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import sys
 
-
+def command_not_found(user_input):
+    print(f"{user_input}: command not found")
 def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
-
+    command=input()
+    command_not_found(command)
 
 if __name__ == "__main__":
     main()
