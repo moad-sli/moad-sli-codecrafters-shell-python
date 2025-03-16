81e3f5c4b9db3f21c61b2e998ea20dd617b501f3
slimane mouad <mslimane@leyton.com>
2025-03-16 22:16:13 +0000

    add command type

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 import sys
+all_command=["exit","echo","type"]
+
+def type_command(user_input):
+    command = user_input.rstrip("type").strip().split(" ")[0]
+    if command in all_command:
+        print(f"{command} is a shell builtin")
+    else:
+        print(f"{command}: not found")
 
 def command_not_found(user_input):
     if user_input == "exit 0":
         return 0
     elif user_input.startswith("echo "):
         print(user_input.removeprefix("echo ").strip())
+        return 1
+    elif user_input.startswith("type"):
+        type_command(user_input)
         return 1
     else:
         print(f"{user_input}: command not found")
