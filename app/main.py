36645023237d840d1e6c29a62fcfe2105501d9c5
slimane mouad <mslimane@leyton.com>
2025-03-23 22:10:38 +0000

    autocomplete

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ import readline
 all_command=["exit","echo","type","pwd","cd"]
 command_path=os.getenv("PATH").split(os.pathsep)
 list_of_paths=os.getenv("PATH").split(os.pathsep)
-options = [command.split("/")[-1]+" " for path in list_of_paths for command in glob.glob(path+"/*") ]+all_command
+options = ([command.split("/")[-1]+" " for path in list_of_paths for command in glob.glob(path+"/*") ]+
+           list(map(lambda  x:x+" ",all_command)))
 
 def complete(text,state):
     matches = [option for option in options if option.startswith(text)]
